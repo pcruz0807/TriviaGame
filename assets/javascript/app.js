bc573340b203a02bbfd9ccc736e3da9e1f5ba77a
pcruz0807 <codecruz86@gmail.com>
2019-01-20 23:59:47 -0800

    trivia pursuit

---
+++ assets/javascript/app.js
@@ -0,0 +1,131 @@
+$(document).ready(function () {
+    var time = 15;
+    $(".reset").click(function() {
+    location.reload(true);
+});
+    $(".start").click(trivia);
+
+    function trivia() {
+        var scoreAry = [];
+        var questions = [{
+            q: "What is Archer’s codename?",
+            s: ["Duchess", "Statesman", "Indigo", "Rainbow"],
+            a: "Duchess",
+            correct: 0
+        }, {
+            q: "Who is Archer's arch nemesis?",
+            s: ["Henry Rollins", "Ray", "Lana", "Barry"],
+            a: "Barry", 
+            correct: 0 
+        }, {
+            q: "What is Archer’s TMNT-inspired alias?",
+            s: ["Tom Smith", "Chet Manley", "Ronnie Michaels", "Andy Garcia"],
+            a: "Chet Manley", 
+            correct: 0 
+        }, {
+            q: "What song is Archer obsessed with?",
+            s: ["Playing with the Boys", "Footloose", "Danger Zone", "I'm Alright"],
+            a: "Danger Zone", 
+            correct: 0 
+
+        }, {
+            q: "Who is NOT one of Archer’s possible fathers?",
+            s: ["Nikolai Jakov", "Len Trexler", "Buddy Rich", "Christian Slater"],
+            a: "Christian Slater", 
+            correct: 0 
+        }, {
+            q: "What is Archer’s middle name?",
+            s: ["James", "Cooper", "Malory", "Ricardo"],
+            a: "Malory", 
+            correct: 0 
+        }, {
+            q: "Who provides the voice for Sterling Archer?",
+            s: ["H. Jon Benjamin", "Dan Mintz", "Larry Murphy", "John Roberts"],
+            a: "H. Jon Benjamin", 
+            correct: 0 
+        }, {
+            q: "Where does Archer escape to after his fiancee’s death?",
+            s: ["French Polynesia", "Siberia", "The Alps", "The Serengeti"],
+            a: "French Polynesia", 
+            correct: 0 
+        }, {
+            q: "Who is the Hollywood starlet who manipulated Archer?",
+            s: ["Bette Davis", "Evelyn Macy", "Marilyn Monroe", "Veronica Deane"],
+            a: "Veronica Deane", 
+            correct: 0 
+        }, {
+            q: "Who is Archer’s hero?",
+            s: ["Robert Redford", "Clint Eastwood", "Burt Reynolds", "Paul Newman"],
+            a: "Burt Reynolds", 
+            correct: 0 
+        }];
+
+        var counter = questions.length;
+
+        //This grabs the question and answer data from the questions array and appends it to the #questions div:
+        function createQuestion(questions) {
+            for (var i = 0; i < questions.length; i++) {
+                $(".start").hide();
+                $("#questions").append('<form id="' + i + '" class="center-text"><p>Question ' + (i + 1) + ' of ' + questions.length + '<br></p><h3 class="question">' + questions[i].q + '</h3><br>' + radioButtons(questions[i].s, i) + ' <br> <button type="submit" class="next">NEXT;</button><br></p></form>');
+            }
+            //This hides all except the first question:
+            for (var k = questions.length - 1; k > 0; k--) {
+                $('#' + k).hide();
+            }
+        }
+        //This grabs the answer choices from the questions array and returns them to createQuestion():
+        function radioButtons(ary, qNum) {
+            var answers = [];
+            for (i = 0; i < ary.length; i++) {
+                answers.push('<label><input type="radio" name="' + qNum + '" value="' + ary[i] + '">' + ary[i] + '</label>');
+            }
+            return answers.join(" ");
+        }
+        
+        //This sums the correct values in the questions array:
+        function sumScore(questions) {
+            return scoreAry.reduce(function (previousValue, currentValue, index, array) {
+                return previousValue + currentValue;
+            });
+        }
+        
+        //This checks the user's answer and updates the score:
+        function checkAnswer(answer, qNum, questions) {
+            if (answer == questions[qNum].a) {
+                questions[qNum].correct = 1;
+                scoreAry.push(questions[qNum].correct);
+            } else {
+                scoreAry.push(questions[qNum].correct);
+            }
+        }
+
+    
+        
+        createQuestion(questions);
+        
+        $(".next").click(function (event) {
+            event.preventDefault(); //This stops the form from submitting
+            var qNum = $(this).closest("form").attr("id"); //This gives us the question number
+            var userInput = $('input[name=' + qNum + ']:radio:checked').val(); //This grabs the user's selected answer
+            if (counter > 1) {
+                checkAnswer(userInput, qNum, questions);
+                $("#" + qNum).hide();
+                $("#" + qNum).next().show();
+                counter--;
+            } else if (counter == 1) {
+                checkAnswer(userInput, qNum, questions);
+                $("#questions").find("form").remove();
+                $("#questions").append('<h3 class="result"></h3>');
+                $(".result").text('You answered ' + sumScore(questions) + ' questions correctly out of 10.');
+                   for (j = 0; j < scoreAry.length; j++) {
+                        if (scoreAry[j] === 0) {
+                            console.log(questions[j].q, questions[j].a);
+                            $("#questions").append('<p class="missed-' + j + '">You missed: ' + questions[j].q + ' ' + questions[j].a + '</p>');      
+                        }
+                    }
+            } else {
+                return false;
+            }
+        });
+    }
+});
